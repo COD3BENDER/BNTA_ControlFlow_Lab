abaa6cee9d9227b23b2969edc441cdc8198bbc73
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-07-06 15:38:12 +0100

    done MVP

--- lab_wk1_d4_controlFlow/src/GuessingGame.java
+++ lab_wk1_d4_controlFlow/src/GuessingGame.java
@@ -1,5 +1,15 @@
 public class GuessingGame {
     public static void main(String[] args) {
+        int secretNumber = 0;
+        int guess = -1;
+
+        if (secretNumber == guess) {
+            System.out.println("Well Done, You are CORRECT!!");
+        } else if (secretNumber < guess) {
+            System.out.println("Your Guess is Too HIGH");
+        } else {
+            System.out.println("Your Guess Is Too LOW");
+        }
 
     }
 }
