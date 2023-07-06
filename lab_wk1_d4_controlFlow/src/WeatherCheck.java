433c60e67978a99df7afc48284c4c965b955a61a
TarekQMUL <t.u.ahmed@se21.qmul.ac.uk>
2023-07-06 15:52:03 +0100

    add Extension

--- lab_wk1_d4_controlFlow/src/WeatherCheck.java
+++ lab_wk1_d4_controlFlow/src/WeatherCheck.java
@@ -1,5 +1,18 @@
 public class WeatherCheck {
     public static void main(String[] args) {
+        int temperature = 21;
+        boolean currentlyRaining = false;
+
+        if (temperature > 20 && !currentlyRaining){
+            System.out.println("wear shorts and sunscreen");
+        } else if (temperature > 20 && currentlyRaining ) {
+            System.out.println("Wear a light rain coat");
+        } else if (temperature < 20 && !currentlyRaining) {
+            System.out.println("wear a jumper/coat it may be cold");
+        }else {
+            System.out.println("your probably better of staying indoors");
+        }
+
 
     }
 }
